7664edb7d333e73dc6a3d5f99cee9987d7b46c5b
Daniel Cruz <dcruz.js@gmail.com>
2020-06-28 07:03:18 -0600

    Updates

---
+++ precache-manifest.2428ce6147e40a9f6f5927af7f727acb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbcc50d4a2539d4af4c975b1658087f",
+    "url": "/invie/index.html"
+  },
+  {
+    "revision": "09a2848b248a74b4964e",
+    "url": "/invie/static/css/main.dd7d020c.chunk.css"
+  },
+  {
+    "revision": "bc4ff4f6436177acf09f",
+    "url": "/invie/static/js/2.06b5f514.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/invie/static/js/2.06b5f514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a2848b248a74b4964e",
+    "url": "/invie/static/js/main.5b6916cc.chunk.js"
+  },
+  {
+    "revision": "66cd4d68f39055364d16",
+    "url": "/invie/static/js/runtime-main.b43d9e5f.js"
+  },
+  {
+    "revision": "cba5fb6368a2ff90d62cc89f9ffefc58",
+    "url": "/invie/static/media/background.cba5fb63.png"
+  },
+  {
+    "revision": "6d0f38aaa66022eef8477e62b86c8283",
+    "url": "/invie/static/media/background2x.6d0f38aa.png"
+  },
+  {
+    "revision": "45598562077bdc84281b5330ff4d283e",
+    "url": "/invie/static/media/corazon.45598562.png"
+  },
+  {
+    "revision": "ba2e415e817351fb402d07afd2be5ceb",
+    "url": "/invie/static/media/easter-a.ba2e415e.png"
+  },
+  {
+    "revision": "7d7a81dcaa54ebefe86d47f90c6bb589",
+    "url": "/invie/static/media/easter-b.7d7a81dc.png"
+  },
+  {
+    "revision": "0db2afe8201f90349fca3d76da2019ef",
+    "url": "/invie/static/media/icomoon.0db2afe8.ttf"
+  },
+  {
+    "revision": "85c2eed24cd21f70d9880d00da02f6bd",
+    "url": "/invie/static/media/icomoon.85c2eed2.eot"
+  },
+  {
+    "revision": "9f215c1e5e1c89f82c1aff021398d605",
+    "url": "/invie/static/media/icomoon.9f215c1e.svg"
+  },
+  {
+    "revision": "e197b52e37ffa7f2e2e6e3d4562c9da4",
+    "url": "/invie/static/media/icomoon.e197b52e.woff"
+  },
+  {
+    "revision": "ec9833fc9629bb65c737f60ba215f4d7",
+    "url": "/invie/static/media/invie-acustica.ec9833fc.png"
+  },
+  {
+    "revision": "6cdc30e08efc87a92750c94de20ddda4",
+    "url": "/invie/static/media/invie-classic.6cdc30e0.png"
+  },
+  {
+    "revision": "70297162b2ac4eb6ac54a0ddd5f85bfe",
+    "url": "/invie/static/media/team-platzi-azul.70297162.jpg"
+  }
+]);
